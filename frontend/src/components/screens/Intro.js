23b85f19fd1fed204315d6019fbf4b98814cdac6
LucasLeiner <lucasleiner05@gmail.com>
2023-12-14 19:54:10 +0100

    Header

--- frontend/src/components/screens/Intro.js
+++ frontend/src/components/screens/Intro.js
@@ -1,8 +1,21 @@
+// Intro.js
+
+import React from 'react';
+import './Intro.css'; // Assure-toi de créer un fichier Intro.css pour y définir les styles
+
 const Intro = () => {
-    return(
-        <>
-            <p>pppppppp</p>
-        </>
+    return (
+        <header className="intro-header">
+            <h1 className="logo">MongoDbtp</h1>
+            <nav className="nav-links">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Entreprises</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
