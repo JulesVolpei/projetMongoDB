535c930c1e8fb274c3296e322aa1d0db666319ba
LucasLeiner <lucasleiner05@gmail.com>
2023-12-14 20:08:52 +0100

    footer pas fini

--- frontend/src/components/screens/Footer.js
+++ frontend/src/components/screens/Footer.js
@@ -1,8 +1,15 @@
+// Footer.js
+
+import React from 'react';
+import './Footer.css'; // Assure-toi de créer un fichier Footer.css pour y définir les styles
+
 const Footer = () => {
-    return(
-        <>
-            <p>mmmmmmm</p>
-        </>
+    return (
+        <footer className="site-footer">
+            <div className="footer-bottom">
+                <p>&copy; 2023 MongoDbtp - Tous droits réservés</p>
+            </div>
+        </footer>
     );
 }
 
