7478675e9014b21b3ed8a0e467718e471974e3f8
DugourdClement <clement.dugourd@etu.univ-amu.fr>
2023-12-15 18:21:15 +0100

    Début tab + modif biblio

--- frontend/src/components/compagniesDetails/ConstructionSiteDetails.js
+++ frontend/src/components/compagniesDetails/ConstructionSiteDetails.js
@@ -1,6 +1,38 @@
-const ConstructionSiteDetails = () => {
-    return(
-        <></>
+import MUIDataTable from 'mui-datatables';
+
+const ConstructionSiteDetails = ({constructionsSite}) => {
+    const columns = [
+        'Chef',
+        'Cout Total',
+        'Facturation',
+        'Nombre Ouvriers',
+        'Date Debut',
+        'Date Fin',
+        'Services',
+    ];
+
+    const options = {
+        filter: true,
+        selectableRows: 'none',
+    };
+
+    const data = constructionsSite.map(construction => [
+        construction.chef,
+        construction.coutTotal,
+        construction.facturation,
+        construction.nombreOuvriers,
+        construction.dateDebut,
+        construction.dateFin,
+        construction.services.join(', '),
+    ]);
+
+    return (
+        <MUIDataTable
+            title={'Construction Site Data'}
+            data={data}
+            columns={columns}
+            options={options}
+        />
     );
 }
 
--- frontend/src/components/map/CompagniesOverview.js
+++ frontend/src/components/map/CompagniesOverview.js
@@ -1,14 +1,20 @@
 import Map from "./Map";
 import CompanyList from "../screens/CompanyList";
 import Grid from "@mui/material/Unstable_Grid2";
-import {makeStyles} from "@material-ui/core/styles";
 
 const CompaniesOverview = () => {
-    const classes = useStyles();
 
     return (
-        <Grid container className={classes.container}>
-            <Grid item xs={3} className={classes.companiesList}>
+        <Grid container sx={{
+            borderRadius: '15px',
+            overflow: 'hidden',
+            border: '1px solid #000',
+            padding: '0.25rem'
+        }}>
+            <Grid item xs={3} sx={{
+                padding: "0 0.25rem 0 !important",
+                borderRight: 'solid black 0.1rem'
+            }}>
                 <CompanyList />
             </Grid>
             <Grid item xs={9}>
@@ -17,18 +23,5 @@ const CompaniesOverview = () => {
         </Grid>
     );
 };
-
-const useStyles = makeStyles({
-    container: {
-        borderRadius: '15px',
-        overflow: 'hidden',
-        border: '1px solid #000',
-        padding: '0.25rem',
-    },
-    companiesList: {
-        padding: "0 0.25rem 0 !important",
-        borderRight: 'solid black 0.1rem',
-    }
-});
 
 export default CompaniesOverview;
