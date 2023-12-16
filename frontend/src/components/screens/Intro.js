2931240a69096a215af6166fdd6b8e5dd2583576
LucasLeiner <lucasleiner05@gmail.com>
2023-12-16 14:32:01 +0100

    Correction css en déclaration dans les balises des composants

--- frontend/src/components/screens/Intro.js
+++ frontend/src/components/screens/Intro.js
@@ -1,51 +1,60 @@
 import React from 'react';
 
 const Intro = () => {
-    return(
-        <header style={styles.introHeader}>
-            <a href="" style={styles.navLinksA}><h1 style={styles.logo}>MongoDbtp</h1></a>
-            <nav style={styles.navLinks}>
-                <ul style={styles.navLinksUl}>
-                    <li style={styles.navLinksLi}><a style={styles.navLinksA} href="entreprise">Entreprises</a></li>
-                    <li style={styles.navLinksLi}><a style={styles.navLinksA} href="services">Services</a></li>
-                    <li style={styles.navLinksLi}><a style={styles.navLinksA} href="contact">Contact</a></li>
+    return (
+        <header style={{
+            backgroundColor: '#e2ac6c',
+            padding: '15px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '1rem',
+        }}>
+            <a href="" style={{
+                textDecoration: 'none',
+                color: '#ab3833',
+                fontWeight: 'bold',
+                fontSize: '16px',
+            }}>
+                <h1 style={{
+                    color: '#ab3833',
+                    margin: '0',
+                    fontSize: '24px',
+                }}>MongoDBTP</h1>
+            </a>
+            <nav>
+                <ul style={{
+                    listStyle: 'none',
+                    display: 'flex',
+                }}>
+                    <li style={{marginRight: '20px',}}>
+                        <a style={{
+                            textDecoration: 'none',
+                            color: '#ab3833',
+                            fontWeight: 'bold',
+                            fontSize: '16px',
+                        }} href="entreprise">Entreprises</a>
+                    </li>
+                    <li style={{marginRight: '20px',}}>
+                        <a style={{
+                            textDecoration: 'none',
+                            color: '#ab3833',
+                            fontWeight: 'bold',
+                            fontSize: '16px',
+                        }} href="services">Services</a>
+                    </li>
+                    <li style={{marginRight: '20px',}}>
+                        <a style={{
+                            textDecoration: 'none',
+                            color: '#ab3833',
+                            fontWeight: 'bold',
+                            fontSize: '16px',
+                        }} href="contact">Contact</a>
+                    </li>
                 </ul>
             </nav>
         </header>
     );
-}
-
-const styles = {
-    introHeader : {
-        backgroundColor: '#e2ac6c',
-        padding: '15px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: '1rem',
-    },
-
-    logo : {
-        color: '#ab3833',
-        margin: '0',
-        fontSize: '24px',
-    },
-
-    navLinksUl : {
-        listStyle: 'none',
-        display: 'flex',
-    },
-
-    navLinksLi : {
-        marginRight: '20px',
-    },
-
-    navLinksA : {
-        textDecoration: 'none',
-        color: '#ab3833',
-        fontWeight: 'bold',
-        fontSize: '16px',
-    },
 }
 
 export default Intro;
